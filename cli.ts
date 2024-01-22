2c58f5dbced4d1b474cd1914af425c2e2263a7e1
Mqxx <62719703+Mqxx@users.noreply.github.com>
2024-01-22 10:59:45 +0100

    Update cli.ts

--- cli.ts
+++ cli.ts
@@ -1,1 +1,7 @@
+import { parseArgs } from "https://deno.land/std@0.212.0/cli/mod.ts";
 
+const args = parseArgs<{
+    path : string,
+    pattern : RegExp,
+    replacer : string | number
+}>(Deno.args);
